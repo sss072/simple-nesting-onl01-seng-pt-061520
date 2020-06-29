1a6ae6c83260a5bbdaf19f6922455b1ae8bb2b87
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-29 00:55:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -110,7 +110,7 @@ def changing_alan
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -125,5 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  programmer_hash = programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
